91fffcd58c92b0659b427dc502a24c58ca8a26ce
= <=>
2022-10-07 18:11:10 +0530

    Tic Tac Toe Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomePageComponent
+  },
+  {
+    path: "**",
+    component: HomePageComponent // Later Change to 404 Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
